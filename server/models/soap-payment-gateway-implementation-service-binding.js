a3c0ea930479980fbaa017ff8e23d25b6109c6c6
Alirëza WJ Arabi <hurricanc@gmail.com>
2017-07-31 13:22:59 +0430

    Create & Save Transaction Data in Transaction Model
    * Call Core Engine Transaction Handler if Verification Response is OK

--- server/models/soap-payment-gateway-implementation-service-binding.js
+++ server/models/soap-payment-gateway-implementation-service-binding.js
@@ -1,8 +1,7 @@
-
 'use strict';
 var server = require('../../server/server');
 
-module.exports = function(PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding) {
+module.exports = function (PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding) {
 
   var soapDataSource = server.datasources.ZPSoapDS;
   var PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding;
@@ -19,198 +18,333 @@ module.exports = function(PaymentGatewayImplementationServicePaymentGatewayImple
    * @callback {Function} callback Callback function
    * @returns {any} callback containing error or result. Result is the response/soap body in JSON form.
    */
-  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentRequest = function(PaymentRequest, callback) {
-      PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentRequest(PaymentRequest, function (err, response) {
+  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentRequest = function (PaymentRequest, callback) {
+    PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentRequest(PaymentRequest, function (err, response) {
+      var transaction = server.models.transaction
+      var data = {
+        "MerchantID": PaymentRequest.MerchantID,
+        "Amount": PaymentRequest.Amount,
+        "Description": PaymentRequest.Description,
+        "Email": PaymentRequest.Email,
+        "Mobile": PaymentRequest.Mobile,
+        "CallbackURL": PaymentRequest.CallbackURL,
+        "Status": response.Status,
+        "Authority": response.Authority
+      }
+      transaction.create(data, function (err, result) {
+        if (err)
+          return callback(err, null)
         var result = response;
         callback(err, result);
-      });
+      })
+    });
   }
-  
+
   /**
    * PaymentRequestWithExtra
    * @param {PaymentRequestWithExtra} PaymentRequestWithExtra PaymentRequestWithExtra
    * @callback {Function} callback Callback function
    * @returns {any} callback containing error or result. Result is the response/soap body in JSON form.
    */
-  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentRequestWithExtra = function(PaymentRequestWithExtra, callback) {
-      PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentRequestWithExtra(PaymentRequestWithExtra, function (err, response) {
-        var result = response;
-        callback(err, result);
-      });
+  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentRequestWithExtra = function (PaymentRequestWithExtra, callback) {
+    PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentRequestWithExtra(PaymentRequestWithExtra, function (err, response) {
+      var result = response;
+      callback(err, result);
+    });
   }
-  
+
   /**
    * PaymentVerification
    * @param {PaymentVerification} PaymentVerification PaymentVerification
    * @callback {Function} callback Callback function
    * @returns {any} callback containing error or result. Result is the response/soap body in JSON form.
    */
-  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentVerification = function(PaymentVerification, callback) {
-      PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentVerification(PaymentVerification, function (err, response) {
-        var result = response;
-        callback(err, result);
-      });
+  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentVerification = function (PaymentVerification, callback) {
+    PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentVerification(PaymentVerification, function (err, response) {
+      var transaction = server.models.transaction
+      transaction.find({
+        'Authority': PaymentVerification.Authority
+      }, function (err, transactionInst) {
+        if (err)
+          return callback(err, null)
+        var data = {
+          "VerificationStatus": response.status,
+          "RefId": response.RefId
+        }
+        transactionInst.updateAttributes(data, function (err, result) {
+          if (err)
+            return callback(err, null)
+
+          var request = require('request')
+
+          function requestToBackend(url, verb, payload, callback) {
+            var options = {
+              method: verb,
+              url: url,
+              preambleCRLF: true,
+              postambleCRLF: true,
+              headers: {
+                'content-type': 'application/json',
+                'accept': 'application/json'
+              },
+              body: JSON.stringify(payload)
+            }
+
+            request(options, function (error, response, body) {
+              if (error || response.statusCode >= 400)
+                return callback(error, null)
+              return callback(null, JSON.parse(body))
+            })
+          }
+          if (response.status === 'OK') {
+            var url = ''
+            var data = {
+              "time": Math.floor((new Date).getTime()),
+              "price": PaymentVerification.Amount,
+              "status": 'Seccessful',
+              "receiptInfo": result,
+              "clientId": result.Description.clientId,
+              "packageId": result.Description.packageId
+            }
+            requestToBackend(url, "POST", data, function (err, result) {
+              if (err)
+                return callback(err, null)
+              return callback(err, response)
+            })
+          } else {
+            var result = response;
+            callback(err, result);
+          }
+        })
+      })
+    });
   }
-  
+
   /**
    * PaymentVerificationWithExtra
    * @param {PaymentVerificationWithExtra} PaymentVerificationWithExtra PaymentVerificationWithExtra
    * @callback {Function} callback Callback function
    * @returns {any} callback containing error or result. Result is the response/soap body in JSON form.
    */
-  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentVerificationWithExtra = function(PaymentVerificationWithExtra, callback) {
-      PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentVerificationWithExtra(PaymentVerificationWithExtra, function (err, response) {
-        var result = response;
-        callback(err, result);
-      });
+  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentVerificationWithExtra = function (PaymentVerificationWithExtra, callback) {
+    PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.PaymentVerificationWithExtra(PaymentVerificationWithExtra, function (err, response) {
+      var result = response;
+      callback(err, result);
+    });
   }
-  
+
   /**
    * GetUnverifiedTransactions
    * @param {GetUnverifiedTransactions} GetUnverifiedTransactions GetUnverifiedTransactions
    * @callback {Function} callback Callback function
    * @returns {any} callback containing error or result. Result is the response/soap body in JSON form.
    */
-  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.GetUnverifiedTransactions = function(GetUnverifiedTransactions, callback) {
-      PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.GetUnverifiedTransactions(GetUnverifiedTransactions, function (err, response) {
-        var result = response;
-        callback(err, result);
-      });
+  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.GetUnverifiedTransactions = function (GetUnverifiedTransactions, callback) {
+    PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.GetUnverifiedTransactions(GetUnverifiedTransactions, function (err, response) {
+      var result = response;
+      callback(err, result);
+    });
   }
-  
+
   /**
    * RefreshAuthority
    * @param {RefreshAuthority} RefreshAuthority RefreshAuthority
    * @callback {Function} callback Callback function
    * @returns {any} callback containing error or result. Result is the response/soap body in JSON form.
    */
-  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.RefreshAuthority = function(RefreshAuthority, callback) {
-      PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.RefreshAuthority(RefreshAuthority, function (err, response) {
-        var result = response;
-        callback(err, result);
-      });
+  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.RefreshAuthority = function (RefreshAuthority, callback) {
+    PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.RefreshAuthority(RefreshAuthority, function (err, response) {
+      var result = response;
+      callback(err, result);
+    });
   }
-  
+
   // Map to REST/HTTP
 
-  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.remoteMethod('PaymentRequest',
-  { isStatic: true,
-  produces: 
-   [ { produces: 'application/json' },
-     { produces: 'application/xml' } ],
-  accepts: 
-   [ { arg: 'PaymentRequest',
-       type: 'PaymentRequest',
-       description: 'PaymentRequest',
-       required: true,
-       http: { source: 'body' } } ],
-  returns: 
-   [ { arg: 'data',
-       type: 'PaymentRequestResponse',
-       description: 'PaymentRequestResponse',
-       root: true } ],
-  http: { verb: 'post', path: '/PaymentRequest' },
-  description: 'PaymentRequest' }
-  );
-  
-  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.remoteMethod('PaymentRequestWithExtra',
-  { isStatic: true,
-  produces: 
-   [ { produces: 'application/json' },
-     { produces: 'application/xml' } ],
-  accepts: 
-   [ { arg: 'PaymentRequestWithExtra',
-       type: 'PaymentRequestWithExtra',
-       description: 'PaymentRequestWithExtra',
-       required: true,
-       http: { source: 'body' } } ],
-  returns: 
-   [ { arg: 'data',
-       type: 'PaymentRequestWithExtraResponse',
-       description: 'PaymentRequestWithExtraResponse',
-       root: true } ],
-  http: { verb: 'post', path: '/PaymentRequestWithExtra' },
-  description: 'PaymentRequestWithExtra' }
-  );
-  
-  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.remoteMethod('PaymentVerification',
-  { isStatic: true,
-  produces: 
-   [ { produces: 'application/json' },
-     { produces: 'application/xml' } ],
-  accepts: 
-   [ { arg: 'PaymentVerification',
-       type: 'PaymentVerification',
-       description: 'PaymentVerification',
-       required: true,
-       http: { source: 'body' } } ],
-  returns: 
-   [ { arg: 'data',
-       type: 'PaymentVerificationResponse',
-       description: 'PaymentVerificationResponse',
-       root: true } ],
-  http: { verb: 'post', path: '/PaymentVerification' },
-  description: 'PaymentVerification' }
-  );
-  
-  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.remoteMethod('PaymentVerificationWithExtra',
-  { isStatic: true,
-  produces: 
-   [ { produces: 'application/json' },
-     { produces: 'application/xml' } ],
-  accepts: 
-   [ { arg: 'PaymentVerificationWithExtra',
-       type: 'PaymentVerificationWithExtra',
-       description: 'PaymentVerificationWithExtra',
-       required: true,
-       http: { source: 'body' } } ],
-  returns: 
-   [ { arg: 'data',
-       type: 'PaymentVerificationWithExtraResponse',
-       description: 'PaymentVerificationWithExtraResponse',
-       root: true } ],
-  http: { verb: 'post', path: '/PaymentVerificationWithExtra' },
-  description: 'PaymentVerificationWithExtra' }
-  );
-  
-  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.remoteMethod('GetUnverifiedTransactions',
-  { isStatic: true,
-  produces: 
-   [ { produces: 'application/json' },
-     { produces: 'application/xml' } ],
-  accepts: 
-   [ { arg: 'GetUnverifiedTransactions',
-       type: 'GetUnverifiedTransactions',
-       description: 'GetUnverifiedTransactions',
-       required: true,
-       http: { source: 'body' } } ],
-  returns: 
-   [ { arg: 'data',
-       type: 'GetUnverifiedTransactionsResponse',
-       description: 'GetUnverifiedTransactionsResponse',
-       root: true } ],
-  http: { verb: 'post', path: '/GetUnverifiedTransactions' },
-  description: 'GetUnverifiedTransactions' }
-  );
-  
-  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.remoteMethod('RefreshAuthority',
-  { isStatic: true,
-  produces: 
-   [ { produces: 'application/json' },
-     { produces: 'application/xml' } ],
-  accepts: 
-   [ { arg: 'RefreshAuthority',
-       type: 'RefreshAuthority',
-       description: 'RefreshAuthority',
-       required: true,
-       http: { source: 'body' } } ],
-  returns: 
-   [ { arg: 'data',
-       type: 'RefreshAuthorityResponse',
-       description: 'RefreshAuthorityResponse',
-       root: true } ],
-  http: { verb: 'post', path: '/RefreshAuthority' },
-  description: 'RefreshAuthority' }
-  );
-  
+  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.remoteMethod('PaymentRequest', {
+    isStatic: true,
+    produces: [{
+        produces: 'application/json'
+      },
+      {
+        produces: 'application/xml'
+      }
+    ],
+    accepts: [{
+      arg: 'PaymentRequest',
+      type: 'PaymentRequest',
+      description: 'PaymentRequest',
+      required: true,
+      http: {
+        source: 'body'
+      }
+    }],
+    returns: [{
+      arg: 'data',
+      type: 'PaymentRequestResponse',
+      description: 'PaymentRequestResponse',
+      root: true
+    }],
+    http: {
+      verb: 'post',
+      path: '/PaymentRequest'
+    },
+    description: 'PaymentRequest'
+  });
+
+  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.remoteMethod('PaymentRequestWithExtra', {
+    isStatic: true,
+    produces: [{
+        produces: 'application/json'
+      },
+      {
+        produces: 'application/xml'
+      }
+    ],
+    accepts: [{
+      arg: 'PaymentRequestWithExtra',
+      type: 'PaymentRequestWithExtra',
+      description: 'PaymentRequestWithExtra',
+      required: true,
+      http: {
+        source: 'body'
+      }
+    }],
+    returns: [{
+      arg: 'data',
+      type: 'PaymentRequestWithExtraResponse',
+      description: 'PaymentRequestWithExtraResponse',
+      root: true
+    }],
+    http: {
+      verb: 'post',
+      path: '/PaymentRequestWithExtra'
+    },
+    description: 'PaymentRequestWithExtra'
+  });
+
+  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.remoteMethod('PaymentVerification', {
+    isStatic: true,
+    produces: [{
+        produces: 'application/json'
+      },
+      {
+        produces: 'application/xml'
+      }
+    ],
+    accepts: [{
+      arg: 'PaymentVerification',
+      type: 'PaymentVerification',
+      description: 'PaymentVerification',
+      required: true,
+      http: {
+        source: 'body'
+      }
+    }],
+    returns: [{
+      arg: 'data',
+      type: 'PaymentVerificationResponse',
+      description: 'PaymentVerificationResponse',
+      root: true
+    }],
+    http: {
+      verb: 'post',
+      path: '/PaymentVerification'
+    },
+    description: 'PaymentVerification'
+  });
+
+  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.remoteMethod('PaymentVerificationWithExtra', {
+    isStatic: true,
+    produces: [{
+        produces: 'application/json'
+      },
+      {
+        produces: 'application/xml'
+      }
+    ],
+    accepts: [{
+      arg: 'PaymentVerificationWithExtra',
+      type: 'PaymentVerificationWithExtra',
+      description: 'PaymentVerificationWithExtra',
+      required: true,
+      http: {
+        source: 'body'
+      }
+    }],
+    returns: [{
+      arg: 'data',
+      type: 'PaymentVerificationWithExtraResponse',
+      description: 'PaymentVerificationWithExtraResponse',
+      root: true
+    }],
+    http: {
+      verb: 'post',
+      path: '/PaymentVerificationWithExtra'
+    },
+    description: 'PaymentVerificationWithExtra'
+  });
+
+  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.remoteMethod('GetUnverifiedTransactions', {
+    isStatic: true,
+    produces: [{
+        produces: 'application/json'
+      },
+      {
+        produces: 'application/xml'
+      }
+    ],
+    accepts: [{
+      arg: 'GetUnverifiedTransactions',
+      type: 'GetUnverifiedTransactions',
+      description: 'GetUnverifiedTransactions',
+      required: true,
+      http: {
+        source: 'body'
+      }
+    }],
+    returns: [{
+      arg: 'data',
+      type: 'GetUnverifiedTransactionsResponse',
+      description: 'GetUnverifiedTransactionsResponse',
+      root: true
+    }],
+    http: {
+      verb: 'post',
+      path: '/GetUnverifiedTransactions'
+    },
+    description: 'GetUnverifiedTransactions'
+  });
+
+  PaymentGatewayImplementationServicePaymentGatewayImplementationServiceBinding.remoteMethod('RefreshAuthority', {
+    isStatic: true,
+    produces: [{
+        produces: 'application/json'
+      },
+      {
+        produces: 'application/xml'
+      }
+    ],
+    accepts: [{
+      arg: 'RefreshAuthority',
+      type: 'RefreshAuthority',
+      description: 'RefreshAuthority',
+      required: true,
+      http: {
+        source: 'body'
+      }
+    }],
+    returns: [{
+      arg: 'data',
+      type: 'RefreshAuthorityResponse',
+      description: 'RefreshAuthorityResponse',
+      root: true
+    }],
+    http: {
+      verb: 'post',
+      path: '/RefreshAuthority'
+    },
+    description: 'RefreshAuthority'
+  });
+
 }
